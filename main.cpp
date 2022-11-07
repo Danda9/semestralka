6e8c46afc837f2c8afa06f7b762c7b62c5be4739
Danda9 <113998003+Danda9@users.noreply.github.com>
2022-11-07 22:41:03 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include "Trenuj.h"
+
+
+
+int main(){
+
+    std::vector <double> omega,ww;
+    std::vector <std::vector <double>> XX;
+    double pr;
+    //int pomocne, y;
+    //int poradi;
+
+    pr = 8.4;
+    //std::vector <vector> X;
+
+    XX = {{2.3,2.9,1},{-2.1,10.6,1},{20.8,21.7,1}};
+    omega = {0,0,1};
+    ww = {5.9,-5.2,pr};
+
+    Trenuj lol = Trenuj(XX,omega,ww);
+    //int pokus = lol.pomocna();
+
+    //std::cout << pokus;
+
+    //for(int i=0; i<40; i++){
+    lol.porovnavej();
+    //}
+    std::cout << XX.size() << "haha\n";
+
+
+//{-2,10,1}
+
+
+
+    //bool kontrola = false;
+
+        //for(int i; i < 4; i++){
+        //poradi = 0; //poradi asi toto spatne funguje""""""""""
+        //for(std:: vector v : X){
+          //  std::cout << "poradi:" << poradi << "\n";
+            //pomocne = v[0]*w[0]+v[1]*w[1]-prah;
+            //if (pomocne<0){
+              //  y=0;
+            //}
+            //else{
+              //  y=1;
+            //}
+            //std::cout << y<< "\n";
+            //if (y!=omega[poradi]){
+            //    w[0] = w[0] + v[0]*(omega[poradi]-y);
+            //    w[1] = w[1] + v[1]*(omega[poradi]-y);
+            //    break;
+            //}
+
+            //poradi=1;
+        //}
+
+        //for (int ii : w) {
+          //  std::cout << w[ii] << " ";
+        //}
+       // std::cout << "czklus\n";
+
+
+    //}
+
+//}
+//    for (std::vector vv : w){
+  //     for (int h : k) {
+    //        std::cout << k[h] << " ";
+        //std::cout << "\n" << std::endl;
+    //}
+      //  std::cout << "\n";
+    //}
+    return 0;
+}
+
